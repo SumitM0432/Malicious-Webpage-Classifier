fab3b30de4fd00f54df05d69660513a61bd5d40a
Sumit Mishra <sumit.mishra0432@gmail.com>
2025-01-25 12:32:06 +0000

    Remove __pycache__ and processing_fns folder from tracking

--- src/processing_fns/count_special.py
+++
@@ -1,8 +0,0 @@
-def count_special(string):
-    count = 0
-    for char in string:
-        if not(char.islower()) and not(char.isupper()) and not(char.isdigit()):
-            if char != ' ':
-                count += 1
-    return count
-# print (count_special('91943reskf bskfb 32rbc9 zvch dfvkdfbv\/.>;?/:{]'))
--- src/processing_fns/tld_extract.py
+++
@@ -1,8 +0,0 @@
-import tld
-from tld import get_tld
-
-def topld(url):
-    res = get_tld(url, fail_silently = True)
-    print (res)
-
-# topld("https://www.google.co.uk")
